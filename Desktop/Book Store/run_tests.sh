58af8f3bea2fb690d4f9337eb7ad0912ed117964
Eya Khalij <eya.khlij@polytechnicien.tn>
2024-12-25 21:50:22 +0100

    Fix test and app functionality

--- Desktop/Book Store/run_tests.sh
+++ Desktop/Book Store/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate virtual environment (optional, if you're using one)
-# source venv/bin/activate
+# source /full/path/to/venv/bin/activate
 
-# Run Flask tests using Python's unittest
+# Run Flask tests using Python's unittest with the full path to the tests directory
 python -m unittest discover -s tests -p "test_flask_app.py"
--- Desktop/Book Store/tests/test_flask_app.py
+++ Desktop/Book Store/tests/test_flask_app.py
@@ -1,14 +1,21 @@
 import unittest
+import os
 from app import app
 from pymongo import MongoClient
 
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+        # Use absolute path for testing purposes
+        current_directory = os.path.abspath(os.path.dirname(__file__))
+        
+        # Set the MongoDB URI (you may need to adjust this if you're using a remote database)
         self.client = MongoClient('mongodb://localhost:27017/')
         self.db = self.client["bookstore"]
         self.books_collection = self.db["books"]
+
+        # Set up the Flask test client
+        self.app = app.test_client()
+        self.app.testing = True
 
     # Test index route
     def test_index(self):
