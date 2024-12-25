0678b3801679794f3bc0b6be56ec68646e1b2708
Eya Khalij <eya.khlij@polytechnicien.tn>
2024-12-25 19:15:18 +0100

    Add run_tests.sh script

---
+++ Desktop/Book Store/run_tests.sh
@@ -0,0 +1,8 @@
+# filepath: /c:/Users/Eya/Desktop/Book Store/run_tests.sh
+#!/bin/bash
+
+# Set environment variables for the test database
+export DB_NAME=test_bookstore
+
+# Run the tests
+python -m unittest discover
